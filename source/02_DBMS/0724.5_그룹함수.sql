baa03e58f4cb51a886ca04eee06a5c1670a8feaf
BJEYEON <jis12030@naver.com>
2023-07-25 17:59:31 +0900

    2023-07-25-1

--- source/02_DBMS/0724.5_그룹함수.sql
+++ source/02_DBMS/0724.5_그룹함수.sql
@@ -47,3 +47,102 @@ SELECT DEPTNO 부서, MAX(SAL) MAX, AVG(SAL) AVG
     FROM EMP
     GROUP BY DEPTNO
     ORDER BY MAX;
+    
+-- EX. 부서명별 최대급여, 평군급여 (부서명순으로 정력)
+SELECT DNAME, MAX(SAL), AVG(SAL)
+    FROM EMP E, DEPT D
+    WHERE E.DEPTNO=D.DEPTNO
+    GROUP BY DNAME;
+--EX. SAL이 5000미만인 사원에 대해서만 부서번호별, 사원수, 최대급여, 최소급여, 평균급여(부서번호 순 정렬)
+SELECT DEPTNO, COUNT(EMPNO), MAX(SAL), MIN(SAL), AVG(SAL) --(3)
+    FROM EMP   --(1)
+    WHERE SAL<5000 --(2)
+    GROUP BY DEPTNO
+    ORDER BY DEPTNO;
+-- ★ 3. HAVING절 : 그룹함수 결과로 제한적으로 출력(필드의 조건을 사용하여 결과를 제한할 때는 WHERE절)
+    -- EX. 부서번호별 평균급여 (평균급여가 2000이상인 부서만 출력)
+SELECT DEPTNO, AVG(SAL)
+    FROM EMP
+    --WHERE AVG(SAL) >= 2000
+    GROUP BY DEPTNO
+    HAVING AVG(SAL)>=2000;
+    --EX.SAL이 5000만원 미만인 사원에 대해 부서명별 최소급여, 평균급여(소숫점 2자리에서 반올림)), 최대급여
+    --단 평균급여가 1800이상인 부서에 대해 평균급여순으로 정렬
+SELECT DNAME, MIN(SAL), ROUND(AVG(SAL), 2), MAX(SAL)
+    FROM EMP E, DEPT D
+    WHERE E.DEPTNO=D.DEPTNO AND SAL<5000
+    GROUP BY DNAME
+    HAVING AVG(SAL)>=1800
+    ORDER BY AVG (SAL);
+    
+--★ 4. 그룹함수 결과의 짐계값 생성
+SELECT NVL(TO_CHAR(DEPTNO), '합'), SUM(SAL) FROM EMP GROUP BY ROLLUP(DEPTNO);
+SELECT DEPTNO, JOB, AVG(SAL) FROM EMP GROUP BY DEPTNO, JOB;
+SELECT DEPTNO, JOB, SUM(SAL) FROM EMP GROUP BY ROLLUP (DEPTNO, JOB);
+
+-- 1. 인원수,최대 급여,최소 급여,급여의 합을 출력
+SELECT COUNT(*), MAX(SAL), MIN(SAL), SUM(SAL)
+    FROM EMP;
+-- 2. 업무별 인원수를 구하여 출력
+SELECT JOB, COUNT(JOB)
+    FROM EMP
+    GROUP BY JOB;
+--- 3. 최고 급여와 최소 급여의 차이는 얼마인가 출력
+SELECT MAX(SAL) - MIN(SAL)
+    FROM EMP;
+    
+-- 4. 각 부서별로 인원수, 급여 평균, 최저 급여, 최고 급여, 급여의 합을 출력(급여의 합이 많은 순으로)
+SELECT JOB, COUNT(JOB), AVG(SAL), MIN(SAL), MAX(SAL), SUM(SAL)
+    FROM EMP
+    GROUP BY JOB
+    ORDER BY SUM(SAL)DESC;
+    
+-- 5. 부서별, 업무별 그룹하여 결과를 부서번호, 업무, 인원수, 급여의 평균, 급여의 합을 출력(부서번호, 업무순으로 오름차순 정렬)
+SELECT DEPTNO, JOB, COUNT(*), AVG(SAL), SUM(SAL)
+    FROM EMP
+    GROUP BY DEPTNO, JOB
+    ORDER BY JOB, DEPTNO;
+-- 6. 업무별, 부서별 그룹하여 결과를  업무, 부서번호, 인원수, 급여의 평균, 급여의 합을 출력(출력결과는 업무순, 부서번호 순 오름차순 정렬)
+SELECT JOB, DEPTNO, COUNT(*), AVG(SAL), SUM(SAL)
+    FROM EMP
+    GROUP BY JOB, DEPTNO
+    ORDER BY JOB, DEPTNO;
+--7. 사원수가 5명이상 넘는 부서번호와 사원수를 출력
+SELECT DEPTNO, COUNT(*)
+    FROM EMP
+    GROUP BY DEPTNO
+    HAVING COUNT(*)>=5;
+-- 8. 사원수가 5명이상 넘는 부서명과 사원수를 출력
+SELECT DNAME, COUNT(ENPNO)
+    FROM EMP E , DEPT D
+    WHERE E.DEPTNO=D.DEPTNO AND COUNT(EMPNO)>=5
+    GROUP BY DNAME;
+    
+    
+--9. 업무별 급여의 평균이 3000이상인 업무에 대해서 업무명, 평균 급여, 급여의 합을 출력
+
+--10. 급여 합이 5000을 초과하는 각 업무에 대해서 업무와 급여합을 출력(급여 합계순 내림차순 정렬)
+
+--11. 부서별 급여평균, 부서별 급여합계, 부서별 최소급여를 출력
+
+--12. 위의 11번을 수정하여, 부서별 급여평균 최대치, 부서별 급여합의 최대치, 부서별 최소급여의 최대치를 출력
+
+--13. 사원 테이블에서 아래의 결과를 출력
+--   H_YEAR	COUNT(*)	MIN(SAL)	MAX(SAL)	AVG(SAL)	SUM(SAL)
+--     80	  1		    800		    800		    800		    800
+--	81	 10		    950		    5000	    2282.5	  22825
+--	82	  2		    1300	    3000	   2150		   4300
+--	83	  1		    1100	    1100	    1100	   1100
+
+-- 14.  아래의 결과를 출력(입사년도별 사원수)
+--  1980     1	
+--  1981     10
+--  1982     2
+--  1983     1
+--  total    14	
+
+--15. 최대급여, 최소급여, 전체급여합, 평균을 출력
+
+--16. 부서별 인원수 출력
+
+--17. 부서별 인원수가 6명이상인 부서번호 출력
