539b2f20a2f67dc7057fbd5b4c97b94985a6743f
BJEYEON <jis12030@naver.com>
2023-08-21 18:04:01 +0900

    2023-08-21-1

---
+++ source/04_JavaScript/ch01/3.js
@@ -0,0 +1,8 @@
+var sum = 0;
+for(var i=1; i<=10; i++){
+    sum += i;
+    console.log("i=" + i + "일때 까지 누적합은" + sum);
+}
+console.log("for문 끝");
+console.log("for문수행후 i의값은" + i);
+console.log("1~10까지 누적합은" + sum);
